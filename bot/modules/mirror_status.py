d91546a1d5f0421ff9f2591542808e46d743d7af
ryzensp <86688030+ryzensp@users.noreply.github.com>
2022-06-13 15:05:28 +0530

    Update mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -15,9 +15,9 @@ def mirror_status(update, context):
         if len(download_dict) == 0:
             currentTime = get_readable_time(time() - botStartTime)
             free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
-            message = 'No Active Downloads !\n___________________________'
-            message += f"\n<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {free}" \
-                       f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {currentTime}"
+            message = '𝐍𝐨 𝐚𝐜𝐭𝐢𝐯𝐞 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐬 !\n___________________________'
+            message += f"\n<b>🖥️ Cᴘᴜ :</b> {cpu_percent()}% | <b>Fʀᴇᴇ:</b> {free}" \
+                       f"\n<b>🎮 Rᴀᴍ :</b> {virtual_memory().percent}% | <b>Uᴘᴛɪᴍᴇ:</b> {currentTime}"
             reply_message = sendMessage(message, context.bot, update.message)
             Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
             return

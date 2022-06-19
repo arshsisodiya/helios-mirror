86a6a3f2270fe0ebb93a8cbe0782880cf416d8aa
Nobody370 <88897873+Nobody370@users.noreply.github.com>
2022-06-19 04:02:28 -0500

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -234,8 +234,8 @@ class MirrorListener:
             msg += f'\n<b>Total Files: </b>{folders}'
             if typ != 0:
                 msg += f'\n<b>Corrupted Files: </b>{typ}'
+            msg += f'\n<b>Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}'    
             msg += f'\n<b>Tag: </b>{self.tag}\n\n'
-            msg += f"\n<b>Elapsed: </b>{get_readable_time(time() - self.message.date.timestamp())}"
             if not files:
                 sendMessage(msg, self.bot, self.message)
             else:
@@ -253,8 +253,8 @@ class MirrorListener:
             if ospath.isdir(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
                 msg += f'\n<b>SubFolders: </b>{folders}'
                 msg += f'\n<b>Files: </b>{files}'
+            msg += f'\n<b>Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}'    
             msg += f'\n\n<b>Tag: </b>{self.tag}'
-            msg += f"\n<b>Elapsed: </b>{get_readable_time(time() - self.message.date.timestamp())}"
             buttons = ButtonMaker()
             link = short_url(link)
             buttons.buildbutton("☁️ Drive Link", link)

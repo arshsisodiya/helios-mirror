4c7fb8ba3401e547b16b6a9f14ec32a1a21396a1
Nobody370 <88897873+Nobody370@users.noreply.github.com>
2022-06-19 04:16:04 -0500

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -234,12 +234,12 @@ class MirrorListener:
             msg += f'\n<b>Total Files: </b>{folders}'
             if typ != 0:
                 msg += f'\n<b>Corrupted Files: </b>{typ}'
-            msg += f"\n<b>Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}"    
+            msg += f'\n<b>Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}'    
             msg += f'\n<b>Tag: </b>{self.tag}\n\n'
             if not files:
                 sendMessage(msg, self.bot, self.message)
             else:
-                fmsg = ''
+                fmsg = '\n<b>Your Files Are:</b>\n\n'
                 for index, (link, name) in enumerate(files.items(), start=1):
                     fmsg += f"{index}. <a href='{link}'>{name}</a>\n"
                     if len(fmsg.encode() + msg.encode()) > 4000:
@@ -253,7 +253,7 @@ class MirrorListener:
             if ospath.isdir(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
                 msg += f'\n<b>SubFolders: </b>{folders}'
                 msg += f'\n<b>Files: </b>{files}'
-            msg += f"\n<b>Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}"    
+            msg += f'\n<b>Elapsed:</b> {get_readable_time(time() - self.message.date.timestamp())}'   
             msg += f'\n\n<b>Tag: </b>{self.tag}'
             buttons = ButtonMaker()
             link = short_url(link)

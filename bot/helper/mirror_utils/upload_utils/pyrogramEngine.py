25e84cffc4e8ee3ed39d2bb4565d3b90a68c09d5
arshsisodiya <arshsisodiya@gmail.com>
2022-06-20 19:12:39 +0530

    [FIX] Fix for prev commit
    While using leech logs bot was giving error while forwarding  Document type files in PM

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -208,7 +208,8 @@ class TgUploader:
                                                                  progress=self.__upload_progress)
                         if BOT_PM:
                             try:
-                                app.send_document(chat_id=self.__user_id, document=self.__sent_msg.document.file_id, caption=cap_mono)
+                                app.send_document(chat_id=self.__user_id, document=self.__sent_msg.document.file_id,
+                                                  caption=cap_mono)
                             except Exception as err:
                                 LOGGER.error(f"Failed To Send Document in PM:\n{err}")
                 else:

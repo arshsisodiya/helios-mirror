9d5a8cee04145d5a262d3725eaf941cfa0277f12
mjwebhacks <98449007+mjwebhacks@users.noreply.github.com>
2022-06-22 09:16:32 +0530

    Update update.py

--- update.py
+++ update.py
@@ -48,8 +48,8 @@ if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 update = srun([f"git init -q \
-                 && git config --global user.email mjwebhacks@gmail.com \
-                 && git config --global user.name mjwebhacks \
+                 && git config --global user.email e.anastayyar@gmail.com \
+                 && git config --global user.name mltb \
                  && git add . \
                  && git commit -sm update -q \
                  && git remote add origin {UPSTREAM_REPO} \

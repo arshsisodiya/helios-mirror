bf836425a75b8bf5808172517ccd73e708f27b19
Nobody370 <88897873+Nobody370@users.noreply.github.com>
2022-06-24 23:26:21 -0400

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -81,7 +81,7 @@ class MirrorListener:
             try:
                 with download_dict_lock:
                     download_dict[self.uid] = ZipStatus(name, m_path, size)
-                path = m_path + ".zip"
+                path = m_path + ".7z"
                 LOGGER.info(f'Zip: orig_path: {m_path}, zip_path: {path}')
                 if self.pswd is not None:
                     if self.isLeech and int(size) > TG_SPLIT_SIZE:

60c05a5ab772e0e1e99e27d2c0e86946f56cd736
arshsisodiya <arshsisodiya@gmail.com>
2022-07-02 21:33:41 +0530

    uncluttering stats
    nuked heroku dyno stats because this branch is not for heroku

--- bot/__init__.py
+++ bot/__init__.py
@@ -495,15 +495,7 @@ try:
 except KeyError:
     APPDRIVE_EMAIL = None
     APPDRIVE_PASS = None
-try:
-    HEROKU_API_KEY = getConfig('HEROKU_API_KEY')
-    HEROKU_APP_NAME = getConfig('HEROKU_APP_NAME')
-    if len(HEROKU_API_KEY) == 0 or len(HEROKU_APP_NAME) == 0:
-        raise KeyError
-except KeyError:
-    LOGGER.warning("Heroku details not entered.")
-    HEROKU_API_KEY = None
-    HEROKU_APP_NAME = None
+
 try:
     TOKEN_PICKLE_URL = getConfig('TOKEN_PICKLE_URL')
     if len(TOKEN_PICKLE_URL) == 0:

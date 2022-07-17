3c577da759aace4a591584b674064c8fcf38c974
Sourcery AI <>
2022-07-17 05:00:27 +0000

    'Refactored by Sourcery'

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -82,7 +82,7 @@ class MirrorListener:
             try:
                 with download_dict_lock:
                     download_dict[self.uid] = ZipStatus(name, m_path, size)
-                path = m_path + ".zip"
+                path = f"{m_path}.zip"
                 LOGGER.info(f'Zip: orig_path: {m_path}, zip_path: {path}')
                 if self.pswd is not None:
                     if self.isLeech and int(size) > TG_SPLIT_SIZE:
@@ -184,7 +184,6 @@ class MirrorListener:
                 reply_to.delete()
             except Exception as error:
                 LOGGER.warning(error)
-                pass
         error = error.replace('<', ' ').replace('>', ' ')
         clean_download(f'{DOWNLOAD_DIR}{self.uid}')
         with download_dict_lock:
@@ -383,7 +382,6 @@ class MirrorListener:
                 reply_to.delete()
             except Exception as error:
                 LOGGER.warning(f"ewww {error}")
-                pass
         e_str = error.replace('<', '').replace('>', '')
         clean_download(f'{DOWNLOAD_DIR}{self.uid}')
         with download_dict_lock:

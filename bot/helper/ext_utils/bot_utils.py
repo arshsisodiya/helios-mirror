1a9c06ec8f6f6232384f636b6dfd0cecf2de5304
Msjdleech <106671274+Msjdleech@users.noreply.github.com>
2022-07-30 08:13:13 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -113,8 +113,8 @@ def get_progress_bar_string(status):
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 8
-    p_str = '▰' * cFull
-    p_str += '▱' * (12 - cFull)
+    p_str = '💝' * cFull
+    p_str += '💟' * (12 - cFull)
     p_str = f"[{p_str}]"
     return p_str
 

799613950597d89e1487b9f5efd2998b866da55c
Msjdleech <106671274+Msjdleech@users.noreply.github.com>
2022-07-31 14:06:31 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -108,13 +108,13 @@ def getAllDownload(req_status: str):
     return None
 
 def get_progress_bar_string(status):
-    completed = status.processed_bytes() / 8
+    completed = status.processed_bytes() / 1
     total = status.size_raw() / 8
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 1
     p_str = '🅸 🅻🅾🆅🅴 🆄' * cFull
-    p_str += '' * (1 - cFull)
+    p_str += '❤️‍🩹' * (1 - cFull)
     p_str = f"[{p_str}]"
     return p_str
 

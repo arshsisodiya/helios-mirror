32f7b81dc6dd3e5784b8deb2fd0abaeb0db501fb
arshsisodiya <arshsisodiya@gmail.com>
2022-08-11 23:29:20 +0530

    fixed appdrive

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -249,7 +249,7 @@ def is_gdtot_link(url: str):
     url = re_match(r'https?://.+\.gdtot\.\S+', url)
     return bool(url)
 def is_appdrive_link(url: str):
-    url = re_match(r'https?://(?:\S*\.)?(?:appdrive|driveapp)\.in/\S+', url)
+    url = re_match(r'https?://(?:\S*\.)?(?:appdrive|driveapp)\.\S+', url)
     return bool(url)
 def is_mega_link(url: str):
     return "mega.nz" in url or "mega.co.nz" in url
